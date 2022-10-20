401c975c0cb9de52e1f767d112d9bcfbf412cd36
junsoo <1herjs1@gmail.com>
2022-10-20 12:01:23 +0900

    update move program

--- Socket_programming/move/move.c
+++ Socket_programming/move/move.c
@@ -11,7 +11,6 @@ int main(int argc, char* argv[]) {
 	int src_fd, dst_fd;
 	int rd_len, wr_len;
 	int total_len = 0;
-	int err_chk;
 	char buf[BUF_SIZE];
 
 	if (argc != 3)
@@ -19,43 +18,23 @@ int main(int argc, char* argv[]) {
 
 	src_fd = open(argv[1], O_RDONLY);
 	if(src_fd == -1)
-		error_handling("open() error!\n");
-
+		error_handling("File not found!\n");
 	dst_fd = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0644);
-	if(dst_fd == -1)
-		error_handling("open() error!\n");
 
-	while (rd_len = read(src_fd, buf, sizeof(buf))) {
-		if(rd_len == -1) 
-			error_handling("read() error!\n");
-
+	while (rd_len = read(src_fd, buf, BUF_SIZE)) {
 		wr_len = write(dst_fd, buf, rd_len);
-		if(wr_len == -1)
-			error_handling("write() error!\n");
-
 		total_len += wr_len;
 	}
 	
-	err_chk = close(src_fd);
-	if(err_chk == -1)
-		error_handling("close() error!\n");
-
-	err_chk = close(dst_fd);
-	if(err_chk == -1)
-		error_handling("close() error!\n");
-
-	err_chk = remove(argv[1]);
-	if(err_chk == -1)
-		error_handling("remove() error!\n");
-
+	close(src_fd);
+	close(dst_fd);
+	remove(argv[1]);
 	printf("move from %s to %s (bytes: %d) finished.\n", argv[1], argv[2], total_len);
-
 	return 0;
 }
 
 void error_handling(char* message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
-
 	exit(1);
 }
